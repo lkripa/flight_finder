5884fa131e954d484456680ea989fa4e84514d69
Alex <alexandra.gg150@gmail.com>
2021-01-20 19:39:31 +0100

    python script to request, filter, and organise flight data given user defined params

---
+++ get_data.py
@@ -0,0 +1,160 @@
+import requests, json
+import sys
+
+def match_skyscanner(city_list):
+    """
+    Match user-defined cities (or countries) with corresponding IDs required by Skyscanner API
+
+    :param city_list: city name (or country), provided by the user
+    
+    Returns Skyscanner API's corresponding ID
+    """
+    city_id = []
+
+    # load database
+    db = []
+
+    # look up ID for each item in list
+    for city in city_list:
+        # look up city.lower(), replace - with space, remove . , extra spaces
+        city_id.append(db[city])
+
+    return city_id
+
+def get_user_params():
+    """
+    Define user preferences 
+
+    Return dict of parameters, and return_trip to indicate whether one-way or round-trip
+    """
+    #default
+    return_trip = False
+
+    # ask user for data
+    origin_list = input('Please provide the two origin cities (separated by a space): ')
+    destination_list = input('Please provide possible destination cities or countries (separated by a space): ')
+    currency = input('What currency would you like to view prices in? [EUR, USD, GBP, CHF]')
+    date_inbound = input('Please provide the desired outbound date (yyyy-mm-dd): ')
+
+    origin_list_ids = match_skyscanner(origin_list)
+    destination_list = match_skyscanner(destination_list)
+
+    # these need to be user defined later on, + match_skyscanner()
+    params = {
+        'origin' : origin_list_ids,
+        'origin_country' : 'ES',
+        'destination' : destination_list_ids,
+        'currency': currency,
+        'locale': 'en-US',
+        'date_outbound' : date_inbound,
+        'date_inbound' : None,
+        'root_url': "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/",
+        'show_flight_info': True
+        }
+    
+    if params['date_inbound'] != None:
+        return_trip = True
+    
+    return params, return_trip
+
+def get_flights(headers, params, return_trip):
+    """
+    Skyscanner API request for every origin and destination provided by user
+        Rate Limit: 50 per minute (unlimited requests)
+
+    Then extracts relevant info from API request, i.e. match airport IDs from Skyscanner 
+        API with their respective names to get inbound and outbound flight info
+
+    :param headers: credentials, provided by RapidAPI
+    :param params: user's preferences
+    :param return_trip: indicates whether one-way or round-trip flight
+
+    Returns list of outbound and inbound flight info, including airport names, 
+            carrier names, and total price of journey
+    """
+    airport_ID = {}
+    carrier_ID = {}
+    outbound = []
+    inbound = []
+
+    for origin in params['origin']:
+        for destination in params['destination']:
+
+            # API request
+            myurl = params['root_url'] + params['origin_country'] + "/" + params['currency'] + "/" + params['locale'] + "/"  + \
+                origin + "/" + destination + "/" + params['date_outbound']
+
+            if return_trip:
+                #querystring = {"inboundpartialdate" : params['date_inbound']}
+                #r = requests.request("GET", myurl, headers=headers, params=querystring)
+                myurl += '/' + params["date_inbound"]
+            
+            r = requests.request("GET", myurl, headers=headers)
+            temp = json.loads(r.text)
+
+            
+            #if "Quotes" in temp: 
+
+            # Extract relevant flight info
+            # build dicts with IDs and Names of airports and carriers
+            for places in temp["Places"]:
+                airport_ID[places["PlaceId"]] = places["Name"] 
+            
+            for carrier in temp["Carriers"]:
+                carrier_ID[carrier["CarrierId"]] = carrier["Name"]
+            
+            # assign route info (with matched names) to outbound and inbound lists
+            for quotes in temp["Quotes"]:
+                origin_outbound = quotes["OutboundLeg"]["OriginId"] #int
+                carrier_outbound = quotes["OutboundLeg"]["CarrierIds"] #list of IDs,int
+                dest_outbound = quotes["OutboundLeg"]["DestinationId"] #int
+                price = quotes["MinPrice"] #int
+
+                outbound.append({"origin": airport_ID[origin_outbound], "destination": airport_ID[dest_outbound], \
+                    "carrier": carrier_ID[carrier_outbound[0]], "price": price})
+
+                if return_trip:
+                    origin_inbound = quotes["InboundLeg"]["OriginId"]
+                    dest_inbound = quotes["InboundLeg"]["DestinationId"]
+                    carrier_inbound = quotes["InboundLeg"]["CarrierIds"]
+                    
+                    inbound.append({"origin": airport_ID[origin_inbound], "destination": airport_ID[dest_inbound], \
+                        "carrier": carrier_ID[carrier_inbound[0]]})
+
+                if params['show_flight_info']:
+                    print("-----------")
+                    print(f"Outbound Journey: {airport_ID[origin_outbound]}  --> {airport_ID[dest_outbound]}")
+                    print(f"Carrier: {carrier_ID[carrier_outbound[0]]}")
+                    print()
+                    if return_trip:
+                        print(f"Inbound Journey:  {airport_ID[origin_inbound]}  --> {airport_ID[dest_inbound]}")
+                        print(f"Carrier: {carrier_ID[carrier_inbound[0]]}")     
+                        print()         
+                    print(f"Total price: {price} EUR")
+                
+    return outbound, inbound
+
+
+def main():
+    headers = {
+        'x-rapidapi-host': "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+        'x-rapidapi-key': "a25c140655mshcc3de02bc48e78fp1d8ae5jsn716b89dc608a"
+        }
+
+    print("Processing user-defined parameters...")
+    params, return_trip = get_user_params()
+
+    print("Requesting flight data...")
+    outbound, inbound = get_flights(headers, params, return_trip)
+    
+    num = len(outbound)
+    print(f"Number of possible flights to be analyzed: {num}")
+
+    print("Done!")
+
+
+if __name__ == "__main__":
+    main()
+
+
+
